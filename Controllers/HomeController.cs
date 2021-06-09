d72172c0a071ec0000d269ddbc0068986e995d1d
Your Name <you@example.com>
2021-06-09 20:25:10 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using OpenXmlPowerTools;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication19.Models;
+
+namespace WebApplication19.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ClinicEntities db = new ClinicEntities();
+  public IEnumerable<EmployeeOfClinic> EmployeeOfClinics { get; set; }
+        public ActionResult Index()
+        {
+
+
+                  
+
+        ViewBag.categories = db.Specialty.ToList();
+    ViewBag.Authors = db.EmployeeOfClinic.ToList();
+            return View();
+
+        }
+
+      
+        private IList<Employee> Getsubcategories(int categoryId)
+        {
+            return db.Employee.Where(m => m.Specialty_ID == categoryId).ToList();
+        }
+        public JsonResult LoadSubCatBycategoryId(int categoryId)
+        {
+            var subcategoriesList = Getsubcategories(categoryId);
+            var subcategoriesData = subcategoriesList.Select(m => new SelectListItem()
+            {
+                Text = m.Name.ToString(),
+                Value = m.Specialty_ID.ToString(),
+            });
+            return Json(subcategoriesData, JsonRequestBehavior.AllowGet);
+        }
+        private IList<Service> Getsubcategories2(int categoryId)
+        {
+            return db.Service.Where(m => m.Specialty_ID == categoryId).ToList();
+        }
+        public JsonResult LoadSubCatBycategoryId2(int categoryId)
+        {
+            var subcategoriesList = Getsubcategories2(categoryId);
+            var subcategoriesData = subcategoriesList.Select(m => new SelectListItem()
+            {
+                Text = m.Name.ToString(),
+                Value = m.Specialty_ID.ToString(),
+            });
+            return Json(subcategoriesData, JsonRequestBehavior.AllowGet);
+        }
+        private IList<EmployeeOfClinic> Getsubcategories3(int categoryId)
+        {
+            
+            return db.EmployeeOfClinic.Where(m => m.EmployeeID == categoryId).ToList();
+        }
+        public JsonResult LoadSubCatBycategoryId3(int categoryId)
+        {
+            var subcategoriesList = Getsubcategories3(categoryId);
+            var subcategoriesData = subcategoriesList.Select(m => new SelectListItem()
+            {
+                Text = m.Clinic.Name.ToString(),
+                Value = m.Employee.id.ToString(),
+            });
+            return Json(subcategoriesData, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+
+}
---
+++ Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.Models
+{
+    public class Context
+    {
+        public IEnumerable<Appointment> Appointments { get; set; }
+        public IEnumerable<Service> Services { get; set; }
+        public IEnumerable<Clinic> Clinics { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
+        public IEnumerable<Client> Clients { get; set; }
+        public IEnumerable<Type> Type { get; set; }
+        public IEnumerable<Specialty> Specialties { get; set; }
+        public IEnumerable<Comments> Comments { get; set; }
+        public IEnumerable<EmployeeOfClinic> EmployeeOfClinics { get; set; }
+    }
+}
